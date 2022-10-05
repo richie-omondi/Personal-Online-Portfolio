d34b6f108e365a4d9e9f6e02dd6568ce44f8c74d
Richard Orido <richieorido@mail.com>
2022-10-05 09:13:46 +0000

    fixed the contact forn

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,20 +13,3 @@ AOS.init({
 
 });
 
-document.querySelector('button').addEventListener('click', (e)=>{
-  e.preventDefault();
-  const contactForm = document.querySelector('#form');
-  const name = document.querySelector('[name="name"]');
-  const email = document.querySelector('[name="email"]');
-  const subject = document.querySelector('[name="subject"]');
-  const content = document.querySelector('[name="content"]');
-  // validation before sending the data
-  if(name.value.length===0 || name.value.length===0 || name.value.length===0){
-    alert('please fill the inputs')
-  }else{
-    let data = new FormData(contactForm);  
-    fetch("_url_here", { method: "POST", body: data });
-    alert('Thank you. your form was submited');
-    contactForm.reset()
-  }
-})
